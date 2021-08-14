567f8333ac057be103c03de4ac9ebbe554bfd4e2
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-08-14 22:43:11 +0100

    added protected routes and wildcard routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./pages/login";
+import NotFound from "./pages/notFound";
+import Index from "./pages/dashboard/index";
+import { Route, Switch } from "react-router-dom";
+import ProtectedRoute from "./auth/protectedRoute";
 
 function App() {
-  return <Login />;
+  return (
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <ProtectedRoute exact path="/dashboard" component={Index} />
+      <Route path="*" component={NotFound} />
+    </Switch>
+  );
 }
 
 export default App;
