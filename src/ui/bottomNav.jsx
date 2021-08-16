a46c7bbfe4f4e32d80af615b01b83525a6a7a590
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-08-16 13:13:24 +0100

    added naviagtion components for mobile and desktop views

---
+++ src/ui/bottomNav.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import Badge from "@material-ui/core/Badge";
+import DashboardTwoToneIcon from "@material-ui/icons/DashboardTwoTone";
+import SendTwoToneIcon from "@material-ui/icons/SendTwoTone";
+import CallReceivedTwoToneIcon from "@material-ui/icons/CallReceivedTwoTone";
+import SettingsInputComponentTwoToneIcon from "@material-ui/icons/SettingsInputComponentTwoTone";
+import NotificationsNoneTwoToneIcon from "@material-ui/icons/NotificationsNoneTwoTone";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles({
+  root: {
+    width: "100vw",
+    backgroundColor: "#eff6ff",
+    overflowX: "scroll",
+  },
+});
+
+export default function MobileBottomNavigation(props) {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const notificationIcon = (
+    <Badge badgeContent={4} color="primary">
+      <NotificationsNoneTwoToneIcon />
+    </Badge>
+  );
+  const history = useHistory();
+  return (
+    <React.Fragment>
+      {props.children}
+      <div className="fixed left-0 bottom-0">
+        <BottomNavigation
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          className={classes.root}
+        >
+          <BottomNavigationAction
+            label="Dashboard"
+            icon={<DashboardTwoToneIcon />}
+            onClick={() => history.push("/account/dashboard")}
+          />
+          <BottomNavigationAction
+            label="Send"
+            icon={<SendTwoToneIcon />}
+            onClick={() => history.push("/account/send")}
+          />
+          <BottomNavigationAction
+            label="Fetch"
+            icon={<CallReceivedTwoToneIcon />}
+            onClick={() => history.push("/account/fetch")}
+          />
+          <BottomNavigationAction
+            label="Settings"
+            icon={<SettingsInputComponentTwoToneIcon />}
+            onClick={() => history.push("/account/settings")}
+          />
+          <BottomNavigationAction
+            label="Notifications"
+            icon={notificationIcon}
+            onClick={() => history.push("/account/notification")}
+          />
+        </BottomNavigation>
+      </div>
+    </React.Fragment>
+  );
+}
